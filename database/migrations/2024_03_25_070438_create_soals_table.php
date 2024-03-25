71122ee4fcd5fb8545ccf74db63cb961a321e1d6
okkyarrohman <okkyzee@gmail.com>
2024-03-25 14:11:20 +0700

    migrations

---
+++ database/migrations/2024_03_25_070438_create_soals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('soals', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('kategori_kuis_Id');
+            $table->foreign('kategori_kuis_Id')->references('id')->on('kategori_kuis');
+            $table->longText('soal');
+            $table->sting('gambar')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('soals');
+    }
+};
